dd07c7ff9169845c0d51306cc06df88463f1344b
Caitlin Schmuck <ces131@pitt.edu>
2019-01-26 11:09:39 -0500

    Camera feature added

--- speed_d8ting/ViewController.swift
+++ speed_d8ting/ViewController.swift
@@ -7,14 +7,86 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    
+    var captureSession = AVCaptureSession()
+    var backCamera:AVCaptureDevice?
+    var frontCamera:AVCaptureDevice?
+    var currentCamera: AVCaptureDevice?
+    var photoOutput: AVCapturePhotoOutput?
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    var image: UIImage?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupCaptureSession()
+        setupDevice()
+        setupInputOutput()
+        setupPreviewLayer()
+        startRunningCaptureSession()
+         //Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func setupCaptureSession(){
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+    }
+    func setupDevice(){
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
+        let devices = deviceDiscoverySession.devices
+        for device in devices{
+            if device.position == AVCaptureDevice.Position.back{
+                backCamera = device
+            }
+            else if device.position == AVCaptureDevice.Position.front{
+                frontCamera = device
+            }
+        }
+        currentCamera = backCamera
+    }
+    
+    func setupInputOutput(){
+        do{
 
+            let captureDeviceInput = try AVCaptureDeviceInput(device: currentCamera!)
+            captureSession.addInput(captureDeviceInput)
+            photoOutput = AVCapturePhotoOutput()
+            photoOutput?.setPreparedPhotoSettingsArray((([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])])), completionHandler: nil)
+            captureSession.addOutput(photoOutput!)
+        }catch{
+            print(error)
+        }
+       
+    }
+    
+    func setupPreviewLayer(){
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        cameraPreviewLayer?.frame = self.view.frame
+        self.view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
+    }
+    
+    func startRunningCaptureSession(){
+        captureSession.startRunning()
+    }
+    
+    @IBAction func captureButton(_ sender: Any) {
+        let settings = AVCapturePhotoSettings()
+        photoOutput?.capturePhoto(with: settings, delegate: self)
+    }
+    
 
 }
-
+extension ViewController: AVCapturePhotoCaptureDelegate{
+    func photoOutput( _ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?){
+        if let imageData = photo.fileDataRepresentation(){
+            print(imageData)
+            image = UIImage(data: imageData)
+            
+            
+        }
+    }
+}
