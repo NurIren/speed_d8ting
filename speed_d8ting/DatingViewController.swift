f8e314ab2b49e12ba7c3158d684628442b3d1622
Nur <nei3@pitt.edu>
2019-01-25 21:18:22 -0500

    the project

---
+++ speed_d8ting/DatingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DatingViewController.swift
+//  speed_d8ting
+//
+//  Created by Nur Iren on 1/25/19.
+//  Copyright © 2019 Nur Iren. All rights reserved.
+//
+
+import UIKit
+
+class DatingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ speed_d8ting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  speed_d8ting
+//
+//  Created by Nur Iren on 1/25/19.
+//  Copyright © 2019 Nur Iren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
