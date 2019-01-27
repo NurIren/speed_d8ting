91f9438b243dc787a2b0e1a0518773c98ac600ad
Nur <nei3@pitt.edu>
2019-01-27 02:11:25 -0500

    Parsing nlp information, some UI updates, passing data between view controllers

--- speed_d8ting/DatingViewController.swift
+++ speed_d8ting/DatingViewController.swift
@@ -7,24 +7,177 @@
 //
 
 import UIKit
+import EventKit
 
-class DatingViewController: UIViewController {
+class DatingViewController: UIViewController{
+    
+    @IBOutlet weak var eventBox: UITextField!
+    
+    @IBOutlet weak var DatBox: UITextField!
+    
+    @IBOutlet weak var TimeBox: UITextField!
+    
+    @IBOutlet weak var VenueBox: UITextField!
+    var date: String!
+    var time: String!
+    var location: String!
+    var eventName: String!
+    var website: String!
+    var monthNum: String!
+    var day: String!
+    var hour: String!
+    var endHour: String!
+    var minute: String!
+    var desc: String!
+    var endMinute: String!
+    
+    @IBOutlet weak var DescTextView: UITextView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBAction func doneButton(_ sender: Any) {
+        //if user presses add event
+        let eventStore = EKEventStore()
+        
+        switch EKEventStore.authorizationStatus(for: .event){
+        case .authorized:
+            insertEvent(store: eventStore)
+        case .denied:
+            print("Access denied")
+        case .notDetermined:
+            //3
+            eventStore.requestAccess(to: .event, completion: {[weak self] (granted: Bool, error: Error?) -> Void in
+                if granted{
+                    self!.insertEvent(store: eventStore)
+                    
+                } else{
+                    print("Access denied")
+                }
+            })
+        default:
+            print("Case default")
+            
+        }
+        self.performSegue(withIdentifier: "addedToCalendar", sender: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        eventBox.text = eventName
+        DatBox.text = monthNum + "/" + day + "/2019"
+        TimeBox.text = hour + ":" + minute + " - " + endHour + ":" + minute
+        VenueBox.text = location
+        //DescTextView.text = DescTextView
+        print("adding to calendar")
+        
+       
+        
     }
-    */
-
+    
+    override func didReceiveMemoryWarning(){
+        super.didReceiveMemoryWarning()
+    }
+    
+    func insertEvent(store: EKEventStore){
+        let calendars = store.calendars(for: .event)
+        
+        for calendar in calendars{
+            if calendar.title == "ioscreator" {
+//                var dateComponents = DateComponents()
+//                dateComponents.year = 2019
+//                dateComponents.month = Int(monthNum)
+//                dateComponents.day = Int(day)
+//                dateComponents.timeZone = TimeZone(abbreviation: "EST")
+//                dateComponents.hour = Int(hour)
+//                dateComponents.minute = Int(minute)
+                
+                //let userCalendar = Calendar.current
+                //let someDataTime = userCalendar.date(from: dateComponents)
+                
+                let formatter = DateFormatter()
+                formatter.dateFormat = "yyyy/MM/dd HH:mm"
+                print(monthNum)
+                print(day)
+                print(hour)
+                print(minute)
+                let someDateTime = formatter.date(from: "2019/" + monthNum + "/" + day + " " + hour + ":" + minute)
+                
+                let startDate = someDateTime
+                //let endDate = startDate.addingTimeInterval(2 * 60 * 60)
+                 let someDateTimeEnd = formatter.date(from: "2019/" + monthNum + "/" + day + " " + endHour + ":" + endMinute)
+                let endDate = someDateTimeEnd
+                
+//                var dateComponentsEnd = DateComponents()
+//                dateComponents.year = 2019
+//                dateComponents.month = Int(monthNum)
+//                dateComponents.day = Int(day)
+//                dateComponents.timeZone = TimeZone(abbreviation: "EST")
+//                dateComponents.hour = Int(endHour)
+//                dateComponents.minute = Int(minute)
+                
+                //let endDate = userCalendar.date(from: dateComponentsEnd)
+                
+                let event = EKEvent(eventStore: store)
+                
+                
+                event.calendar = calendar
+                print(website)
+                if(website != nil){
+                    do {
+                        let url = try NSURL(string: website)
+                        try event.url = url as! URL
+                    } catch{
+                        print("no url found")
+                    }
+                }
+                    do{
+                        try event.title = eventName
+                    } catch{
+                        print("No event name")
+                    }
+                
+                
+                
+                //event.startDate = someDataTime
+                do{
+                    try event.startDate = startDate
+                    try event.endDate = endDate
+                } catch{
+                    let startdate = Date()
+                    
+                    let endDate = startdate.addingTimeInterval(2 * 60 * 60)
+                    event.startDate = startdate
+                    event.endDate = endDate
+                    
+                }
+                
+                do{
+                    try event.location = location
+                } catch{
+                    print("no event location found")
+                }
+                
+                //event.description = desc
+                
+                
+                do{
+                    try store.save(event, span: .thisEvent)
+                }
+                catch{
+                    print("Error saving event in calendar")
+                }
+            }
+        }
+    }
 }
+
+
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+
--- speed_d8ting/ViewController.swift
+++ speed_d8ting/ViewController.swift
@@ -13,6 +13,10 @@ import NaturalLanguage
 
 class ViewController: UIViewController, G8TesseractDelegate {
     
+    @IBAction func nextButton(_ sender: Any) {
+        performSegue(withIdentifier: "toSummary", sender: self);
+        print("now here")
+    }
     
     var captureSession = AVCaptureSession()
     var backCamera:AVCaptureDevice?
@@ -22,6 +26,13 @@ class ViewController: UIViewController, G8TesseractDelegate {
     var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
     var image: UIImage?
     //@IBOutlet weak var textView: UITextView!
+    var dates = [String]()
+    var location : String!
+    var website: String!
+    var eventName: String!
+    var monthStuff = [String]()
+    var timeStuff = [String]()
+    
     
     let tokenizer = NLTokenizer(unit: .word)
     
@@ -88,6 +99,190 @@ class ViewController: UIViewController, G8TesseractDelegate {
     @IBAction func captureButton(_ sender: Any) {
         let settings = AVCapturePhotoSettings()
         photoOutput?.capturePhoto(with: settings, delegate: self)
+        print("here")
+        //let newViewController = DatingViewController()
+        //self.navigationController?.pushViewController(newViewController, animated: true)
+        //sleep(30)
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let secondView = segue.destination as! DatingViewController
+        secondView.location = location
+        secondView.eventName = eventName
+        secondView.website = website
+        //____________________________
+        monthStuff = toMonthDay(stringIn: dates[0])
+        if(monthStuff.count == 0){
+            secondView.monthNum = ""
+            secondView.day = ""
+        }else if(monthStuff.count == 1){
+            secondView.monthNum = ""
+            secondView.day = ""
+        }else if(monthStuff.count == 2){
+            secondView.monthNum = monthStuff[0]
+            secondView.day = monthStuff[1]
+        }else{
+            secondView.monthNum = monthStuff[0]
+            secondView.day = monthStuff[1]
+        }
+        
+        timeStuff = toProperTime(str1: dates[1])
+        if(timeStuff.count == 0){
+            secondView.hour = "00"
+            secondView.minute = "00"
+            secondView.endHour = "00"
+            secondView.endMinute = "00"
+        } else {
+            secondView.hour = timeStuff[0]
+            secondView.minute = timeStuff[1]
+            secondView.endHour = timeStuff[2]
+            secondView.endMinute = timeStuff[3]
+        }
+    
+        //_______________________________
+        
+    }
+    func matches(for regex: String, in text: String) -> [String] {
+        
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let results = regex.matches(in: text,
+                                        range: NSRange(text.startIndex..., in: text))
+            return results.map {
+                String(text[Range($0.range, in: text)!])
+            }
+        } catch let error {
+            print("nothing")
+            return []
+        }
+    }
+    func toProperTime(str1: String) -> [String]{
+        var result = [String]()
+        let matched = matches(for: "(\\d)?\\d(:\\d{2})?(A|a|p|P)(M|m)", in: str1 )
+        var tempHour = 0
+        
+        for m in matched{
+            var num = m.count
+            var test = m
+            var newNum = 0
+            var mTime = false
+            var tempMin = "00"
+            
+            if(m.contains(":")){
+                if m.count == 7 {
+                    //it's 10,11,or 12 PM
+                    var i = m.index(m.startIndex, offsetBy: num-6)
+                    tempHour = Int(String(m[i]))! + 10
+                    tempMin = String(m.dropFirst(3))
+                }
+                else{
+                    var i = m.index(m.startIndex, offsetBy: num-6)
+                    tempHour = Int(String(m[i]))!
+                    tempMin = String(m.dropFirst(2))
+                }
+                
+                tempMin = String(tempMin.dropLast(2))
+                
+            }
+            else{
+                if m.count == 4{
+                    var i = m.index(m.startIndex, offsetBy: num-3)
+                    tempHour = Int(String(m[i]))! + 10
+                }
+                else{
+                    var i = m.index(m.startIndex, offsetBy: num-3)
+                    tempHour = Int(String(m[i]))!
+                }
+            }
+            let index = m.index(m.startIndex, offsetBy: num-2)
+            //doesn't work for NOON!!!
+            
+            if (String(m[index]).uppercased() == "P") {
+                tempHour = tempHour + 12
+            }
+            var finalHour = String(tempHour)
+            
+            result.append(finalHour)
+            result.append(tempMin)
+            
+        }
+        
+        if(result.count == 2){
+            result.append(String(tempHour + 2))
+            result.append("00")
+        }
+        return result
+    }
+    func toMonthDay(stringIn: String) -> [String] {
+        var charset = CharacterSet.letters
+        var month = ""
+        var day = ""
+        var year = "2019"
+        if stringIn.lowercased().rangeOfCharacter(from: charset) != nil {
+            var mdy = stringIn.split(separator: " ")
+            var string: String = String(mdy[0]).lowercased()
+            day = String(mdy[1])
+            day = day.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+            if day.count < 2 {
+                day = "0" + day
+            }
+            if(string.contains("jan")){
+                month = "01"
+            }
+            else if(string.contains("feb")){
+                month = "02"
+            }
+            else if(string.contains("mar")){
+                month = "03"
+            }
+            else if(string.contains("apr")){
+                month = "04"
+            }
+            else if(string.contains("may")){
+                month = "05"
+            }
+            else if(string.contains("jun")){
+                month = "06"
+            }
+            else if(string.contains("jul")){
+                month = "07"
+            }
+            else if(string.contains("aug")){
+                month = "08"
+            }
+            else if(string.contains("sep")){
+                month = "09"
+            }
+            else if(string.contains("oct")){
+                month = "10"
+            }
+            else if(string.contains("nov")){
+                month = "11"
+            }
+            else if(string.contains("dec")){
+                month = "12"
+            }
+        }
+        else {
+            var mdy = stringIn.split(separator: "/")
+            month = String(mdy[0])
+            day = String(mdy[1])
+            
+            if mdy.count > 2 {
+                year = String(mdy[2])
+            }
+            if year.count < 3 {
+                year = "20" + year
+            }
+            if month.count < 2 {
+                month = "0" + month
+            }
+            if day.count < 2 {
+                day = "0" + day
+            }
+        }
+        var date = [month, day, year]
+        return date
     }
     
 
@@ -111,8 +306,15 @@ extension ViewController: AVCapturePhotoCaptureDelegate{
                 tokenizer.string = text
                 let tagger = NSLinguisticTagger(tagSchemes: [.tokenType], options: 0)
                 tagger.string = text
+                var count = 0
                 tokenizer.enumerateTokens(in: text.startIndex..<text.endIndex) { tokenRange, _ in
+                    if( count == 0){
+                        eventName = String(text[tokenRange])
+                    }else if(count == 1){
+                        eventName = eventName + " " + String(text[tokenRange])
+                    }
                     print(text[tokenRange])
+                    count += 1
                     return true
                 }
                 let options: NSLinguisticTagger.Options = [.omitPunctuation, .omitWhitespace, .joinNames]
@@ -144,6 +346,7 @@ extension ViewController: AVCapturePhotoCaptureDelegate{
                     if match?.resultType == .date {
                         
                         print("date: \(matchString)")
+                        dates.append(matchString)
                         
                     }else if match?.resultType == .phoneNumber {
                         
@@ -153,11 +356,13 @@ extension ViewController: AVCapturePhotoCaptureDelegate{
                     }else if match?.resultType == .address {
                         
                         print("address: \(matchString)")
+                        location = matchString
                         
                         
                     }else if match?.resultType == .link {
                         
                         print("link: \(matchString)")
+                        website = matchString
                         
                         
                     }else{
@@ -167,5 +372,34 @@ extension ViewController: AVCapturePhotoCaptureDelegate{
                 })
             }
         }
+        
     }
+}
+extension StringProtocol {
+    
+    subscript(offset: Int) -> Element {
+        return self[index(startIndex, offsetBy: offset)]
+    }
+    
+    subscript(_ range: CountableRange<Int>) -> SubSequence {
+        return prefix(range.lowerBound + range.count)
+            .suffix(range.count)
+    }
+    subscript(range: CountableClosedRange<Int>) -> SubSequence {
+        return prefix(range.lowerBound + range.count)
+            .suffix(range.count)
+    }
+    
+    subscript(range: PartialRangeThrough<Int>) -> SubSequence {
+        return prefix(range.upperBound.advanced(by: 1))
+    }
+    subscript(range: PartialRangeUpTo<Int>) -> SubSequence {
+        return prefix(range.upperBound)
+    }
+    subscript(range: PartialRangeFrom<Int>) -> SubSequence {
+        return suffix(Swift.max(0, count - range.lowerBound))
+    }
+}
+extension Substring {
+    var string: String { return String(self) }
 }
